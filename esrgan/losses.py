db2129d5c350e5b6943a3df1c505e0ca877e1361
supertramp2 <nivedpa@am.students.amrita.edu>
2021-07-02 13:34:42 +0530

    tf-gan style

--- esrgan/losses.py
+++ esrgan/losses.py
@@ -1,14 +1,13 @@
-
 import tensorflow as tf
+
 
 def pixel_loss(y_true, y_pred):
     y_true = tf.cast(y_true, tf.float32)
     y_pred = tf.cast(y_pred, tf.float32)
-
     return tf.reduce_mean(tf.reduce_mean(tf.abs(y_true - y_pred), axis = 0))
 
 
-def RalitivisticAverageLoss(non_trans_disc, type_ = "G"):
+def RealitivisticAverageLoss(non_trans_disc, type_ = "G"):
     loss = None
 
     def D_Ra(x, y):
@@ -28,6 +27,7 @@ def RalitivisticAverageLoss(non_trans_disc, type_ = "G"):
     def loss_G(y_true, y_pred):
         real_logits = D_Ra(y_true, y_pred)
         fake_logits = D_Ra(y_pred, y_true)
+        
         real_loss = tf.nn.sigmoid_cross_entropy_with_logits(
             labels=tf.zeros_like(real_logits), logits=real_logits)
         fake_loss = tf.nn.sigmoid_cross_entropy_with_logits(
@@ -35,7 +35,6 @@ def RalitivisticAverageLoss(non_trans_disc, type_ = "G"):
         
         return real_loss + fake_loss
     
-
     return loss_G if type == "G" else loss_D
 
 
@@ -45,8 +44,18 @@ def PerceptualLoss(weight = None, input_shape = None, loss_type = "L1"):
     )
 
     for layer in vgg_model.layers:
-        layers.trainable = False
+        layer.trainable = False
+    
     vgg_model.get_layer("block5_conv4").activation = lambda x: x
     phi = tf.keras.Model(
         inputs = [vgg_model.input],
         outputs =[vgg_model.get_layer("block5_conv4").output])
+    
+    def loss(y_true, y_pred):
+      if loss_type.lower() == "l1":
+        return tf.compat.v1.losses.absolute_difference(phi(y_true), phi(y_pred))
+
+      if loss_type.lower() == "l2":
+        return tf.reduce_mean(
+            tf.reduce_mean((phi(y_true) - phi(y_pred))**2, axis=0))
+    return loss
